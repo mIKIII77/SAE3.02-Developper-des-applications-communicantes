3dcc8e4eec05632a711309ea8ed2a69096759296
Mikhail KARAPETYAN <89877252+mIKIII77@users.noreply.github.com>
2022-11-28 23:22:35 +0100

    V.4

--- Backend/clientgui.py
+++ Backend/clientgui.py
@@ -17,41 +17,56 @@ class MainWindow(QMainWindow):
         grid = QGridLayout()
         widget.setLayout(grid)
         self.setWindowTitle("SuperVision")
-        self.setGeometry(100, 100, 1600, 1000)
-        
+        self.setGeometry(100, 100, 700, 700)
+
         #Items
         self.connect = QPushButton("Add new server")
         self.listservers = QListWidget()
-        text = QLabel("Les serveurs enrégistrés :")
-        serverreply = QTextEdit()
+        self.text = QLabel("Les serveurs enrégistrés :")
+        
+        # Readonly QPlainTextEdit for the logs
+        self.serverreply = QPlainTextEdit()
+        self.serverreply.setReadOnly(True)
+        #readonly
+
         self.inputip = QLineEdit()
         self.inputip.setPlaceholderText("Enter IP address")
         self.inputport = QLineEdit()
         self.inputport.setPlaceholderText("Enter port")
-        inputcommand = QLineEdit()
-        inputcommand.setPlaceholderText("Enter command")
-        sendcommand = QPushButton("Send command")
-        presavebuttontypeos = QPushButton("Type OS")
-        presavebuttonram = QPushButton("RAM Info")
-        presavebuttonhostname = QPushButton("Hostname")
+        self.inputcommand = QLineEdit()
+        self.inputcommand.setPlaceholderText("Enter command")
+        self.sendcommand = QPushButton("Send command")
+        self.presavebuttontypeos = QPushButton("Type OS")
+        self.presavebuttonram = QPushButton("RAM Info")
+        self.presavebuttonhostname = QPushButton("Hostname")
 
         #Layout
-        grid.addWidget(text, 0, 0)
+        grid.addWidget(self.text, 0, 0)
         grid.addWidget(self.listservers, 1, 0 , 1, 3)
         grid.addWidget(self.inputip, 2, 0)
         grid.addWidget(self.inputport, 2, 1)
         grid.addWidget(self.connect, 2, 2)
-        grid.addWidget(serverreply, 3, 0, 1, 3)
-        grid.addWidget(inputcommand, 4, 0, 1, 2)
-        grid.addWidget(sendcommand, 4, 2, 1, 2)
-        grid.addWidget(presavebuttontypeos, 5, 0)
-        grid.addWidget(presavebuttonram, 5, 1)
-        grid.addWidget(presavebuttonhostname, 5, 2, 1, 2)
+        grid.addWidget(self.serverreply, 3, 0, 1, 3)
+        grid.addWidget(self.inputcommand, 4, 0, 1, 2)
+        grid.addWidget(self.sendcommand, 4, 2, 1, 2)
+        grid.addWidget(self.presavebuttontypeos, 5, 0)
+        grid.addWidget(self.presavebuttonram, 5, 1)
+        grid.addWidget(self.presavebuttonhostname, 5, 2, 1, 2)
 
         #Events
         self.connect.clicked.connect(self.__connect)
         self.listservers.itemDoubleClicked.connect(self.__chose_server)
+        self.sendcommand.clicked.connect(self.__send_data)
+        self.presavebuttontypeos.clicked.connect(self.__typeos)
+        self.presavebuttonram.clicked.connect(self.__raminfo)
+        self.presavebuttonhostname.clicked.connect(self.__hostname)
 
+
+        
+        
+
+        
+        
         #Functions
     def __connect(self):
         if self.inputip.text() == "" or self.inputport.text() == "":
@@ -61,8 +76,12 @@ class MainWindow(QMainWindow):
             self.listservers.addItem(self.inputip.text() + ":" + self.inputport.text())
             self.inputip.setText("")
             self.inputport.setText("")
-    
+
+
+
+
     def __chose_server(self):
+        global client
         if self.listservers.currentItem() == None:
             QMessageBox.warning(self, "Error", "Please select a server")
         else:
@@ -72,23 +91,57 @@ class MainWindow(QMainWindow):
                 port = int(self.listservers.currentItem().text().split(":")[1])
                 client.connect((ip, port))
                 QMessageBox.information(self, "Success", "Connected to server")
+                threading.Thread(target=self.__receive_data, args=[client]).start()
+                return activeconnection == True
             except:
                 QMessageBox.warning(self, "Error", "Connection failed")
-                return
-    def __print_server_reply(self):
-        reply = client.receive_data(client)
-        # Write reply in serverreply QTextEdit
-        
-            
+                return client
 
 
-            
+    def __receive_data(self, client):
+        while True:
+            data = client.recv(1024).decode('utf-8')
+            #Write data to QPlainTextEdit at new line
+            self.serverreply.appendPlainText(data)
+            #write
+            if data == "close":
+                client.close()
+                break
+      
 
-if __name__ == '__main__':
+    def __send_data(self, client):
+        if self.inputcommand.text() == "":
+            QMessageBox.warning(self, "Error", "Please enter a command")
+        elif activeconnection == False:
+            QMessageBox.warning(self, "Error", "Please select a server")
+        else:
+            client.send(self.inputcommand.text().encode('utf-8'))
+            self.inputcommand.setText("")
+
+    def __typeos(self):
+        self.inputcommand.setText("os")
+        self.__send_data()
+
+    def __raminfo(self):   
+        self.inputcommand.setText("ram")
+        self.__send_data()
+
+    def __hostname(self):
+        self.inputcommand.setText("hostname")
+        self.__send_data()
+
+
+def main():
+    global activeconnection
+    activeconnection = False
     app = QApplication(sys.argv)
     window = MainWindow()
     window.show()
     sys.exit(app.exec_())
+  
 
-    
+if __name__ == '__main__':
+    main()
+
+
 
--- Backend/server.py
+++ Backend/server.py
@@ -4,17 +4,18 @@ import platform
 
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.bind(('localhost', 10018))
+server.bind(('localhost', 10019))
 server.listen(5)
 os = platform.system()
 
- # Automatically accept the connection of many supervisors
+# Automatically accept the connection of many supervisors
 def accept_clients():
+    global client
     while True:
         print('Waiting for connection...')
         client, addr = server.accept()
         print(f'Admin with, {addr} connected') 
-        threading.Thread(target=receive_data, args=[client]).start()
+        client.send(f'Welcome to the server! You are connected to {os}.'.encode('utf-8'))
 
 
 def receive_data(client):
@@ -26,6 +27,11 @@ def receive_data(client):
             elif data == 'close':
                 client.close()
                 break
+
+def send_data(client):
+    while True:
+        data = input('Enter command: ')
+        client.send(data.encode('utf-8'))
 
 def main():
     threading.Thread(target=accept_clients, args=[]).start()
